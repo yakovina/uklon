1d79501a2dc4eefc738f983857a165772cf4712e
Oleksandra Yakovyna <oleksandra.yakovyna@playtika.com>
2022-10-19 13:32:16 +0300

    add first situation:

--- src/features/quiz/components/Header.tsx
+++ src/features/quiz/components/Header.tsx
@@ -1,14 +1,18 @@
 import styles from '../Quiz.module.css'
 import Logo from '../img/logo.png';
-import {Pidtrymka} from './Pidtrymka';
+import LogoHmarochos from '../img/hmarochos_logo_white.svg';
 
 export const Header = () => {
 
     return<>
-        <Pidtrymka />
     <div className={styles.header}>
+        <a href="https://uklon.com.ua/" className={styles.logoHmarochos}>
+            <img src={LogoHmarochos} alt="Hmarochos" />
+        </a>
+
         <a href="https://uklon.com.ua/" className={styles.logo}>
         <img src={Logo} alt="UKLON" />
+            <span>Партнерська<br/>публікація</span>
         </a>
     </div>
     </>
--- src/features/quiz/components/Pidtrymka.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '../Quiz.module.css'
-import PLASH from '../img/plash.png';
-
-export const Pidtrymka = () => {
-
-    return <a href="#" className={styles.plash}>
-            <img src={PLASH} alt={'Партнерський матеріал'} />
-        </a>
-}
